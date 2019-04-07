c35e53c36bce4d4877ced890c7751173aef3fd30
Suhoy571 <isuhovienko@gmail.com>
2019-04-07 18:30:18 +0300

    Построение меню

--- views/site/index.php
+++ views/site/index.php
@@ -4,7 +4,7 @@
 
 $this->title = 'My Yii Application';
 ?>
-
+<?= \app\components\MenuWidget::widget(['tpl' => 'menu']) ?>
 <section id="slider"><!--slider-->
     <div class="container">
         <div class="row">
@@ -79,7 +79,6 @@ $this->title = 'My Yii Application';
             <div class="col-sm-3">
                 <div class="left-sidebar">
                     <h2>Category</h2>
-                    <?= \app\components\MenuWidget::widget(['tpl' => 'menu']) ?>
 
                     <div class="panel-group category-products" id="accordian"><!--category-productsr-->
                         <div class="panel panel-default">
