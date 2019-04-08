0bc2f1840dfa5574ef94551162167006487670d9
Suhoy571 <isuhovienko@gmail.com>
2019-04-08 15:44:07 +0300

    Получение меню из базы и его кэширование

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -3,6 +3,7 @@
 
 namespace app\components;
 
+use Yii;
 use yii\base\Widget;
 use app\models\Category;
 
@@ -11,7 +12,7 @@ class MenuWidget extends Widget
     public $tpl;
     public $data; //Массив данных категориий из базы
     public $tree; //Результат работы функции, построение дерава категорий
-    public $menuHTML; //Готовый HTML-код дерева
+    public $menuHtml; //Готовый HTML-код дерева
 
     public function init()
     {
@@ -24,12 +25,22 @@ class MenuWidget extends Widget
 
     public function run()
     {
+        //Проверка на наличие кэша
+        $menu = Yii::$app->cache->get('menu');
+        //Данные есть - возвращаем
+        if ($menu)
+            return $menu;
+        //Данных в кэше нет - формируем его
         $this->data = Category::find()->indexBy('id')->asArray()->all();
         $this->tree = $this->getTree();
         $this->menuHtml = $this->getMenuHtml($this->tree);
+
+        //set cache
+        Yii::$app->cache->set('menu', $this->menuHtml, 60 * 60);
         return $this->menuHtml;
     }
 
+    //Проход в цикле по массиву и стоит дерево
     protected function getTree()
     {
         $tree = [];
@@ -53,6 +64,7 @@ class MenuWidget extends Widget
 
     protected function catToTemplate($category)
     {
+        //Буферизация
         ob_start();
         include __DIR__ . '/menu_tpl/' . $this->tpl;
         return ob_get_clean();
