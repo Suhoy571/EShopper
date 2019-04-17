5a3791e48733524430b7c1cf820efadeda29f64b
Suhoy571 <isuhovienko@gmail.com>
2019-04-17 22:38:44 +0300

    Просмотр товаров категорий

--- views/category/view.php
+++ views/category/view.php
@@ -55,22 +55,28 @@ $this->title = 'My Yii Application';
             </div>
 
             <div class="col-sm-9 padding-right">
-                <?php if (!empty($products)): ?>
-                    <div class="features_items"><!--features_items-->
-                        <h2 class="title text-center">Features Items</h2>
+                <div class="features_items"><!--features_items-->
+                    <h2 class="title text-center">Features Items</h2>
+                    <?php if (!empty($products)): ?>
                         <?php $i = 0;
-                        foreach ($products as $product) : ?>
+                        foreach ($products as $product): ?>
                             <div class="col-sm-4">
                                 <div class="product-image-wrapper">
                                     <div class="single-products">
                                         <div class="productinfo text-center">
-                                            <?= Html::img("@web/images/products/{$product->img}", ['alt' => $product->name]) ?>
-                                            <h2>$<?= $product->price; ?></h2>
-                                            <p><?= $product->name; ?></p>
+                                            <?= Html::img("@web/images/products/{$product->img}", ['alt' => $hit->name]) ?>
+                                            <h2>$<?= $product->price ?></h2>
+                                            <p><?= $product->name ?></p>
                                             <a href="#" class="btn btn-default add-to-cart"><i
-                                                        class="fa fa-shopping-cart"></i>Add
-                                                to cart</a>
+                                                        class="fa fa-shopping-cart"></i>Add to cart</a>
                                         </div>
+                                        <!--<div class="product-overlay">
+                                            <div class="overlay-content">
+                                                <h2>$56</h2>
+                                                <p>Easy Polo Black Edition</p>
+                                                <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                            </div>
+                                        </div>-->
                                         <?php if ($product->new): ?>
                                             <?= Html::img("@web/images/home/new.png", ['alt' => 'Новинка', 'class' => 'new']) ?>
                                         <?php endif ?>
@@ -86,26 +92,23 @@ $this->title = 'My Yii Application';
                                     </div>
                                 </div>
                             </div>
-                            <?php $i++;
-                            if ($i % 3 == 0): ?>
+                            <?php $i++ ?>
+                            <?php if ($i % 3 == 0): ?>
                                 <div class="clearfix"></div>
                             <?php endif; ?>
                         <?php endforeach; ?>
-                    </div> <!--features_items-->
-                <?php else: ?>
-                    <h2>Здесь товаров пока нет...</h2>
-                <?php endif; ?>
-
-                <div class="clearfix"></div>
-
-                <ul class="pagination">
-                    <li class="active"><a href="">1</a></li>
-                    <li><a href="">2</a></li>
-                    <li><a href="">3</a></li>
-                    <li><a href="">&raquo;</a></li>
-                </ul>
-            </div><!--features_items-->
+                    <?php else : ?>
+                        <h2>Здесь товаров пока нет...</h2>
+                    <?php endif; ?>
+                    <div class="clearfix"></div>
+                    <ul class="pagination">
+                        <li class="active"><a href="">1</a></li>
+                        <li><a href="">2</a></li>
+                        <li><a href="">3</a></li>
+                        <li><a href="">&raquo;</a></li>
+                    </ul>
+                </div><!--features_items-->
+            </div>
         </div>
-    </div>
     </div>
 </section>
