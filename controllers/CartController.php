d78fac38696384efabce2cfe07dc320fe94b1982
Suhoy571 <isuhovienko@gmail.com>
2019-05-21 11:51:38 +0300

    Офрмление заказа

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -111,9 +111,39 @@ class CartController extends AppController
         $session->open();
         $this->setMeta('Корзина');
         $order = new Order();
+
+        //Загрузка данных пришедших с формы
         if ($order->load(Yii::$app->request->post())) {
-            debug(Yii::$app->request->post());
+            $order->qty = $session['cart.qty'];
+            $order->qty = $session['cart.sum'];
+            if ($order->save()) {
+                $this->saveOrderItems($session['cart'], $order->id);
+                Yii::$app->session->setFlash('success', 'Заказ принят');
+                //Очистка корзины
+                $session->remove('cart');
+                $session->remove('cart.qty');
+                $session->remove('cart.sum');
+                return $this->refresh();
+            } else {
+                Yii::$app->session->setFlash('error', 'Ошибка оформления заказа');
+            }
         }
+
         return $this->render('view', compact('session', 'order'));
+    }
+
+    protected function saveOrderItems($items, $order_id)
+    {
+        //Проход в цикле по массиву корзины
+        foreach ($items as $id => $item) {
+            $order_items = new OrderItems();
+            $order_items->order_id = $order_id;
+            $order_items->product_id = $id;
+            $order_items->name = $item['name'];
+            $order_items->price = $item['price'];
+            $order_items->qty_item = $item['qty'];
+            $order_items->sum_item = $item['qty'] * $item['price'];
+            $order_items->save();
+        }
     }
 }
